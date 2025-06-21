6618a5fd6c8c24b25d9a7047e9d4f2a2d61b4984
Peter Almanzar <peter.almanzar@gmail.com>
2025-06-21 15:10:21 -0400

    ...

--- attending-medical-ai/apps/backend/Attending.Presentation.DataGather.CMD/Program.cs
+++ attending-medical-ai/apps/backend/Attending.Presentation.DataGather.CMD/Program.cs
@@ -1,36 +1,40 @@
-﻿namespace Attending.Presentation.DataGather.CMD;
+﻿using Attending.Presentation.DataGather.CMD.Sources.FamilyPracticeNoteBook;
+
+namespace Attending.Presentation.DataGather.CMD;
 
 internal static class Program
 {
-    private const string _familyPracticeBaseUrl = "https://fpnotebook.com";
-
     static async Task Main(string[] args)
     {
-        using var httpClient = new HttpClient()
-        {
-            BaseAddress = new Uri(_familyPracticeBaseUrl)
-        };
-
-        // Symptoms
-        var symptoms = await getSymptoms(httpClient);
-        await getSymptomsDetails(httpClient, symptoms);
-        await saveSymptomsToCsv(symptoms);
-
-        // Examinations
-        var examinations = await getExaminations(httpClient);
-        await getExaminationsDetails(httpClient, examinations);
-        await saveExaminationsToCsv(examinations);
-
-        // Medications
-        var medications = await getMedications(httpClient);
-        await getMedicationsDetails(httpClient, medications);
-        await saveMedicationsToCsv(medications);
-
-        // Procedures
-        var procedures = await getProcedures(httpClient);
-        await getProceduresDetails(httpClient, procedures);
-        await saveProceduresToCsv(procedures);
-
+        await getDataFromFamilyPracticeNoteBook();
         Console.WriteLine("Data gathering completed successfully.");
+    }
+
+    private static async Task getDataFromFamilyPracticeNoteBook()
+    {
+        var symptoms = await FamilyPracticeNotebookScraperService.GetSymptoms();
+        if(symptoms.Any())
+            await CsvPersistenceService.saveToCsv(symptoms, "Symptoms");
+        else
+            Console.WriteLine("No symptoms found.");
+
+
+        var examinations = await FamilyPracticeNotebookScraperService.GetExaminations();
+        if(examinations.Any())
+            await CsvPersistenceService.saveToCsv(examinations, "Examinations");
+        else
+            Console.WriteLine("No examinations found.");
+
+        var medications = await FamilyPracticeNotebookScraperService.GetMedications();
+        if(medications.Any())
+            await CsvPersistenceService.saveToCsv(medications, "Medications");
+        else
+            Console.WriteLine("No medications found.");
+
+        var procedures = await FamilyPracticeNotebookScraperService.GetProcedures();
+        if(procedures.Any())
+            await CsvPersistenceService.saveToCsv(procedures, "Procedures");
+        else
+            Console.WriteLine("No procedures found.");
     }
 }
